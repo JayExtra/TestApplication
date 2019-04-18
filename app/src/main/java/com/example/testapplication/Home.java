975b1976330e629915e572079e4a432a0ee7d11a
JayExtra <jaymotosh@gmail.com>
2019-04-18 18:15:10 +0300

    Test Update

--- app/src/main/java/com/example/testapplication/Home.java
+++ app/src/main/java/com/example/testapplication/Home.java
@@ -3,6 +3,7 @@ package com.example.testapplication;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationMenu;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -27,12 +28,29 @@ public class Home extends AppCompatActivity {
 
         bottomNavigationViewHome.setOnNavigationItemSelectedListener(navListener);
 
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            return false;
+            Fragment selectedFragment=null;
+
+            switch(menuItem.getItemId()){
+                case R.id.nav_ranking:
+                    selectedFragment =new RankingsFragment();
+                    break;
+
+                case R.id.nav_home:
+                    selectedFragment =new HomeFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
         }
     };
 }
